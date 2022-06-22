d3bdc35ea500d72a7edd570d389db087a72a8139
BrawnyClover <sonbill971@gmail.com>
2022-06-22 19:03:04 +0900

    Master-Slave 관계 구상

---
+++ master_process.py
@@ -0,0 +1,10 @@
+import ArrowDetection
+import AlphabetDetection
+
+# @Todo
+# opencv로 카메라로부터 이미지 가져오기
+# 이미지 전처리
+# Contour 리스트 생성 후 ArrowDetection 모듈과 AlphabetDetection 모듈로 리스트 넘겨줌
+# from ArrowDetection, only receive arrow (isArrow, direction)
+# from AlphabetDetection, only receive (isAlphabet, what charecter is)
+# if this process can be dealt with MPMD(1 core for Alphabet detection, 1 core for Arrow detection), that would be best
